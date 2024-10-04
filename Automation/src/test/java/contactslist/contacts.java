b3f024814d67f299a6d32254bdbce48857c2a81e
Admin <Admin@DESKTOP-5L0A769>
2024-10-04 13:09:07 +0530

    firstpush1

---
+++ Automation/src/test/java/contactslist/contacts.java
@@ -0,0 +1,10 @@
+package contactslist;
+
+public class contacts {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ Automation/src/test/java/oraganizationlist/organization.java
@@ -0,0 +1,9 @@
+package oraganizationlist;
+
+public class organization {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
